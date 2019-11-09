8de255f5d898e683aca83ec48214efe9b9558ed2
dh1914218 <dh1914218@gmail.com>
2019-11-09 14:35:26 +0900

    g

--- js/script.js
+++ js/script.js
@@ -650,3 +650,25 @@ $(".estimate_selector h1").click(function() {
   $(".m_arrow").hide();
   $(".estimate.mixsale").hide();
 });
+
+// 핸드폰번호 자동하이픈
+$(".phone").keydown(function(event) {
+  var key = event.charCode || event.keyCode || 0;
+  $text = $(this);
+  if (key !== 8 && key !== 9) {
+    if ($text.val().length === 3) {
+      $text.val($text.val() + "-");
+    }
+    if ($text.val().length === 8) {
+      $text.val($text.val() + "-");
+    }
+  }
+
+  return (
+    key == 8 ||
+    key == 9 ||
+    key == 46 ||
+    (key >= 48 && key <= 57) ||
+    (key >= 96 && key <= 105)
+  );
+});
